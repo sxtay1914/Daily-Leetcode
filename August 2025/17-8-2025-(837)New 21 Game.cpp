d85be1216e31e9fdd2df5353be78a1695e3711a4
sxtay1914 <167632565+sxtay1914@users.noreply.github.com>
2025-08-17 13:17:01 +0000

    Added POTD 17-8

---
+++ August 2025/17-8-2025-(837)New 21 Game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+//O(N)
+//O(N)
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if (k<=0) return 1.0;
+        vector<double> dp(n+1);
+        double wsum = (k>0)? 1: 0, res = 0;
+        dp[0] = 1;
+        //sliding window is from 1 to maxPts
+        //
+        for (int i=1; i<=n; i++){
+            dp[i] = wsum/maxPts;
+            if (i<k){
+                wsum += dp[i];
+            }else{
+                res += dp[i];
+            }
+
+            if (i-maxPts>=0 && i-maxPts<k){
+                wsum -= dp[i-maxPts];
+            }
+        }
+        return res;
+    }
+};
